787139b549812024a845614c4756182c4f4c43d8
Nayan chandrakar <chandrakarnayan96@gmail.com>
2023-11-16 14:14:46 +0530

    some improvements

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,13 +8,15 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard(process.env.NEXT_PUBLIC_ID!);
+  const billboard = await getBillboard();
+
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard 
-          data={billboard}
+        // @ts-ignore
+          data={billboard?.[0]}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
